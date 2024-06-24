e8469d5d0fe0b419c9408d6e3f43b13a6c0a42a6
sumansauravmay <sumansauravmay12345@gmail.com>
2024-06-25 01:21:23 +0530

    deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
